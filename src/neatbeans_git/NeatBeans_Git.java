a4abd96c8cfc36931981af60213dafc66e109570
usuari <usuari@10.0.2.15>
2022-05-04 18:17:34 +0200

    Version 1

---
+++ src/neatbeans_git/NeatBeans_Git.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package neatbeans_git;
+
+/**
+ *
+ * @author usuari
+ */
+public class NeatBeans_Git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+    }
+    
+}
